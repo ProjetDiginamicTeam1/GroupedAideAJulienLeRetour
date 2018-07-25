8de2f37e8b313c86e017c97fad31a1d06b6623d3
Unknown <julien.thiallet@gmail.com>
2018-07-25 16:40:45 +0200

    project !!

---
+++ ProjetTacos/ProjetTacos/Models/Tacos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetTacos.Models
+{
+    public class Tacos
+    {
+        public int Id { get; set; }
+        public string Viande { get; set; }
+        public string Boisson { get; set; }
+    }
+}
